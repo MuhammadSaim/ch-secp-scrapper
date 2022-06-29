153f4379441365a9a9b5007adff85cecf1991613
Muhammad Saim <muhammadsaim494@gmail.com>
2022-06-30 00:00:51 +0500

    Remove chrome driver & refector code to selenum >= 4.0.0

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
@@ -9,15 +11,18 @@ search_keys = [
 
 
 def scrape():
-    driver = webdriver.Chrome('./drivers/chromedriver')
-    driver.get('https://eservices.secp.gov.pk/eServices/NameSearch.jsp')
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    browser.get('https://eservices.secp.gov.pk/eServices/NameSearch.jsp')
+
+    # check "Including Exact String" radio
+    browser.find_element(By.CSS_SELECTOR, "input[type='radio'][value='Including Exact String']").click()
 
     # find the searchbar in the page
-    search_bar = driver.find_element(By.NAME, 'searchName')
+    search_bar = browser.find_element(By.NAME, 'searchName')
     search_bar.clear()
     search_bar.send_keys('Air')
     search_bar.send_keys(Keys.RETURN)
-    driver.close()
+    browser.close()
 
 
 if __name__ == '__main__':
